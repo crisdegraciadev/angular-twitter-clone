7f11cefd11b7a17c06e087ead5bae0bb19593df0
Cristian De Gracia Nuero <crisdegraciadev@gmail.com>
2023-03-03 17:38:37 +0100

    feat(layout): Add navbar smart component & necessary ui components

---
+++ src/app/layout/ui/account-selector/account-selector.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-account-selector',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './account-selector.component.html',
+  styleUrls: ['./account-selector.component.css']
+})
+export class AccountSelectorComponent {
+
+}
--- src/app/layout/ui/navbar/types/navbar-options.type.ts
+++
@@ -1,6 +0,0 @@
-import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
-
-export type NavbarOption = {
-  icon: IconDefinition;
-  value: string;
-};
